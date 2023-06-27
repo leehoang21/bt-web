3a0fa811f6bc41a8acd588ad2c7cc367c1382f5c
hoanglee21 <hoanglethanh2132003@gmail.com>
2023-06-27 18:20:48 +0700

    api product mobile

--- database/migrations/2023_06_08_033946_create_admins_table.php
+++ database/migrations/2023_06_08_033946_create_admins_table.php
@@ -16,10 +16,10 @@ return new class extends Migration
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
-            $table->string('user_name')->unique();
+            $table->string('user_name')->unique()->default('admin');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->default('123456');
             $table->rememberToken();
             $table->timestamps();
         });
