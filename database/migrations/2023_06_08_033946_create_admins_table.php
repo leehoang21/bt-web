b7242ede7cf0d289eeb0892021e8be9e175b826a
hoanglee21 <hoanglethanh2132003@gmail.com>
2023-06-27 18:38:30 +0700

    init user admin

--- database/migrations/2023_06_08_033946_create_admins_table.php
+++ database/migrations/2023_06_08_033946_create_admins_table.php
@@ -16,10 +16,10 @@ return new class extends Migration
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
-            $table->string('user_name')->unique()->default('admin');
+            $table->string('user_name')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->default('123456');
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
